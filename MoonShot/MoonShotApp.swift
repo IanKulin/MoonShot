8b5caeceff50c5b41e1dc8f61f3fd4448b22f595
Ian Bailey <IanKulin@kulin.com.au>
2022-10-08 06:04:30 +0800

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Ian Bailey on 8/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
