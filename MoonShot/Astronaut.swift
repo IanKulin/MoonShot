0a4689358c0bbea77b36caa2ebb1b105266d49b4
Ian Bailey <IanKulin@kulin.com.au>
2022-10-09 07:54:18 +0800

    Tutorial version

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Ian Bailey on 8/10/2022.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
